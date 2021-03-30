e8d239c65a4b88746480a38f15240bd5bf119147
cavilosa <cavilosa33@gmail.com>
2021-03-30 10:36:22 -0600

    created show class with one to many relations

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -39,8 +39,11 @@ class Venue(db.Model):
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    shows = db.relationship('Show', backref = 'venue', lazy=True)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -55,6 +58,16 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    shows = db.relationship('Show', backref = 'artists', lazy=True)
+
+
+class Show(db.Model):
+    __tablename__="Show"
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey("Artist.id"), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey("Venue.id"), nullable=False)
+
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
