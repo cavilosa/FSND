1e1958268cf3c17bb92bd000980c4bd954df7756
cavilosa <cavilosa33@gmail.com>
2021-03-30 09:53:29 -0600

    config file updated, flask init, migrate and inital uograde done

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -6,8 +6,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
-# Connect to the database
+# Connect to the database  db = SQLAlchemy()
 
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:cavilosa1@localhost:5432/fyyur'
