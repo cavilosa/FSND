03385f1191fe76243fd79bbce3dae3353514bf74
cavilosa <cavilosa33@gmail.com>
2021-06-11 09:23:28 -0600

    added comments to api.py

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -11,23 +11,10 @@ app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
-'''
-@TODO uncomment the following line to initialize the datbase
-!! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
-!! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
-!! Running this funciton will add one
-'''
+# drops and creates db and a drink object for postman testing
 db_drop_and_create_all()
 
-'''
-@TODO implement endpoint
-    GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
-
+# this route available for all users, down't require permissionsm shows short drinks information
 @app.route("/drinks")
 def get_drinks():
     drinks_full_list = Drink.query.all()
@@ -42,15 +29,7 @@ def get_drinks():
         "drinks": drinks
     })
 
-
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+# the route shows detailed information about drinks, requires authorization, availabele for barista and manager, not for puclic use
 @app.route("/drinks-detail")
 @requires_auth("get:drinks-detail")
 def get_drinks_detail(payload):
@@ -64,15 +43,7 @@ def get_drinks_detail(payload):
         "drinks": drinks
     })
 
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
+# the route to post a new drink, requires permission, role - manager only
 @app.route("/drinks", methods = ["POST"])
 @requires_auth("post:drinks")
 def post_drink(payload):
@@ -98,17 +69,7 @@ def post_drink(payload):
     })
 
 
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
+# requires manager permission to change detailes in existing drink from the db
 @app.route("/drinks/<id>", methods=["PATCH"])
 @requires_auth("patch:drinks")
 def patch_drink(payload, id):
@@ -142,18 +103,7 @@ def patch_drink(payload, id):
         "drinks": [drink.long()]
     })
 
-
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
-
+# a route to delete a drink for the db by its id, requires manager permission
 @app.route("/drinks/<id>", methods=["DELETE"])
 @requires_auth("delete:drinks")
 def delete_drink(payliad, id):
@@ -170,10 +120,6 @@ def delete_drink(payliad, id):
 
 
 # Error Handling
-'''
-Example error handling for unprocessable entity
-'''
-
 
 @app.errorhandler(422)
 def unprocessable(error):
@@ -198,26 +144,3 @@ def auth_error(error):
         "error": error.error,
         "code": error.status_code
     }), error.status_code
-
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False,
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
-
-'''
-
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above
-     ({'code': 'unauthorized', 'description': 'Permission not found.'}, 403)
-'''
-
-
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above
-'''
